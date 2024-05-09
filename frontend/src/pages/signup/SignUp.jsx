6a6395109054fc94b9c6390db23692a2c57bf0f0
ehsan304 <muhammadehsan304@gmail.com>
2024-05-09 22:50:13 +0500

    Signup functionality didnt work so i delete the code and now, i will write it again

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
 
+
 const SignUp = () => {
+
+    
+
     return (
         <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
             <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -13,14 +18,17 @@ const SignUp = () => {
                         <label className='label p-2'>
                             <span className='text-base label-text'>Full Name</span>
                         </label>
-                        <input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
+                        <input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10'
+                        />
                     </div>
 
                     <div>
                         <label className='label p-2 '>
                             <span className='text-base label-text'>Username</span>
                         </label>
-                        <input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+                        <input type='text' placeholder='johndoe' className='w-full input input-bordered h-10'
+                    
+                        />
                     </div>
 
                     <div>
@@ -31,6 +39,7 @@ const SignUp = () => {
                             type='password'
                             placeholder='Enter Password'
                             className='w-full input input-bordered h-10'
+                        
                         />
                     </div>
 
@@ -42,14 +51,15 @@ const SignUp = () => {
                             type='password'
                             placeholder='Confirm Password'
                             className='w-full input input-bordered h-10'
+                        
                         />
                     </div>
 
-                    <GenderCheckbox />
+                    <GenderCheckbox/>
 
-                    <a className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' href='#'>
+                    <Link className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block' to='/login'>
                         Already have an account?
-                    </a>
+                    </Link>
 
                     <div>
                         <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
